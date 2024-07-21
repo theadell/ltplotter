85083e2ae7d144f33c59d034463d21b4738f7f0e
theadell <65723807+theadell@users.noreply.github.com>
2024-07-21 17:39:15 +0200

    Add pgfplot Service to docker compose

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -5,30 +5,45 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"log/slog"
 	"ltplotter/pkg/mapper"
 	"ltplotter/pkg/parser"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 )
 
-var port int
-var host string
+type Config struct {
+	Port               int
+	Host               string
+	PGFPlotsServiceURL string
+}
 
 func main() {
+	config := &Config{}
 
-	flag.IntVar(&port, "port", 8080, "TCP Port to bind server to")
-	flag.StringVar(&host, "host", "localhost", "Network to bind to")
+	flag.IntVar(&config.Port, "port", 8080, "TCP Port to bind server to")
+	flag.StringVar(&config.Host, "host", "localhost", "Network to bind to")
+
 	flag.Parse()
+
+	config.PGFPlotsServiceURL = os.Getenv("PGFPLOT_SVC_URL")
+	proxy, err := createReverseProxy(config.PGFPlotsServiceURL)
+	if err != nil {
+		log.Printf("Warning: Failed to parse PGFPlots service URL: %v\n", err)
+	}
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /api/v1/parse", parseSimulationHandler)
+	mux.HandleFunc("POST /api/v1/pgfplot", createPGFPlotHandler(proxy))
 
 	server := http.Server{
-		Addr:              fmt.Sprintf("%s:%d", host, port),
+		Addr:              fmt.Sprintf("%s:%d", config.Host, config.Port),
 		Handler:           mux,
 		ReadTimeout:       time.Second * 5,
 		ReadHeaderTimeout: time.Second * 2,
@@ -39,7 +54,7 @@ func main() {
 	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
 
 	go func() {
-		slog.Info(fmt.Sprintf("Server is running on port %d", port))
+		slog.Info(fmt.Sprintf("Server is running on port %d", config.Port))
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			slog.Error("Server failed to start", "err", err.Error())
 			os.Exit(1)
@@ -78,4 +93,36 @@ func parseSimulationHandler(w http.ResponseWriter, r *http.Request) {
 		slog.Error("failed to encode parsed data into json", "error", err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func createPGFPlotHandler(proxy *httputil.ReverseProxy) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("RECIEVED A REQUEST", "host", r.Host, "path", r.URL.Path, "url", r.URL)
+		if proxy == nil {
+			http.Error(w, "Service Unavailable", http.StatusServiceUnavailable)
+			return
+		}
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+func createReverseProxy(targetURL string) (*httputil.ReverseProxy, error) {
+	if targetURL == "" {
+		return nil, fmt.Errorf("target URL is empty")
+	}
+
+	parsedURL, err := url.Parse(targetURL)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(parsedURL)
+	proxy.Director = func(r *http.Request) {
+		r.URL.Scheme = parsedURL.Scheme
+		r.URL.Host = parsedURL.Host
+		r.URL.Path = "/generate-plot"
+		r.Host = parsedURL.Host
+		slog.Info("proxy passing request", "url", r.URL.String())
+	}
+	return proxy, nil
 }
