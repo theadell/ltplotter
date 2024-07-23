485a69881870487656d109567c2b8050b6570ce8
duseo <marius.roser@googlemail.com>
2024-07-23 15:54:33 +0200

    Add logging to pgf plot service

--- pgfPlotService/Program.cs
+++ pgfPlotService/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +10,13 @@ builder.Services.AddSingleton<PlotService.Services.PlotService>();
 
 var app = builder.Build();
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
 app.MapPost("/generate-plot", async (HttpContext context, PlotRequest request, PlotService.Services.PlotService plotService) =>
 {
+    Log.Information("Received PlotRequest {@plotRequest}", request);
     var latexString = plotService.GenerateLatex(request);
     var pdf = plotService.CompileLatex(latexString);
     context.Response.ContentType = "application/pdf";
