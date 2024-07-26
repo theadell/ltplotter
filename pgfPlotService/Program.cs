ec8c8683ca05b73abf1d79cc882a80bf49642f03
duseo <marius.roser@googlemail.com>
2024-07-26 18:46:56 +0200

    Try codegen

--- pgfPlotService/Program.cs
+++ pgfPlotService/Program.cs
@@ -2,12 +2,26 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
+using PgfPlot;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<PlotService.Services.PlotService>();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGrpcService<PgfPlotService>();
+
+    endpoints.MapGet("/", async context =>
+    {
+        await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+    });
+});
 
 app.MapPost("/generate-plot", async (HttpContext context, PlotRequest request, PlotService.Services.PlotService plotService) =>
 {
