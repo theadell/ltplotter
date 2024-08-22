c8d654ee5459d772abab2e02ada7f41b19fd1314
Adel <65723807+theadell@users.noreply.github.com>
2024-08-22 09:24:27 +0200

    ui: set basic theme

--- frontend/src/typed-router.d.ts
+++ frontend/src/typed-router.d.ts
@@ -19,6 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/[...path]': RouteRecordInfo<'/[...path]', '/:path(.*)', { path: ParamValue<true> }, { path: ParamValue<false> }>,
     '/data': RouteRecordInfo<'/data', '/data', Record<never, never>, Record<never, never>>,
   }
 }
